1b277c5b7e731b8a5fb0f80c763ba7d465672f6b
Tillen otuoma <totuoma27@gmail.com>
2024-11-26 10:55:06 +0300

    "Removed leading slash from image src paths in three HTML files"

---
+++ learn_javascript.js
@@ -0,0 +1,11 @@
+// var myName = "Tillen"
+// let mySecondName = "Otuoma"
+// const myLocation = "Kampala"
+
+// console.log(myName)
+// console.log(mySecondName.length)
+// console.log(myLocation.toUpperCase())
+
+let username = "tillen"
+let password = 12345
+
