c9944f057cca10ea5476c53ba7dc113652449db1
Tillen otuoma <totuoma27@gmail.com>
2024-11-27 14:05:58 +0300

    created javascript files

---
+++ hobby.js
@@ -0,0 +1,21 @@
+
+let otherHobby = "Photography";
+
+function myHobby(hobbyName, frequency, description) {
+
+    let majorHobby = hobbyName; 
+    let howOften = frequency;    
+
+    if (true) { 
+        let hobbyBenefits = description; 
+        let ocassionalHobby = "nature trails"; 
+
+        return `My hobby is ${majorHobby}. I engage in this activity ${howOften}. ${hobbyBenefits} I also enjoy ${ocassionalHobby} occasionally.`;
+    }
+
+}
+
+let myHobbyDescription = myHobby("Adventure", "every weekend", "It allows me to express my creativity and relax my mind.");
+console.log(myHobbyDescription);
+
+console.log(`Another hobby of mine is ${otherHobby}.`);
